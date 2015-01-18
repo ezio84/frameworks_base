27b5abd8a5cba0a214e9825008d716904224cc68
Danesh M <daneshm90@gmail.com>
2015-01-18 00:33:00 -0800

    Lockscreen: customizable bottom left and right shortcuts [2/2]
    Thanks CM!!
    
    Change-Id: I4abf1deed0a342e111453516fa7820191a55ffc4
    
    Conflicts:
    	packages/SystemUI/Android.mk
    	packages/SystemUI/res/layout/keyguard_bottom_area.xml
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    SystemUI : Move shortcut helper instantiation before updateVisibility
    
    Change-Id: Idbb97116b243dc9ddbffec6bc020d15fa59d90b9
    
    SystemUI : Fix Rtl for lockscreen shortcuts
    
    Change-Id: I2c3c72bb6e1b06402b882b4f8b2d8d250d813bcd
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Cleanup & optimization.
    
    Change-Id: I2815e814301788cfb1fbf68abb7de947b85c1669
    
    SystemUI : Add ability to disable lockscreen targets
    
    Change-Id: Iec684028a3056de47163058c44c0cfc252098fe1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -44,8 +44,8 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.util.cm.LockscreenShortcutsHelper;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSPanel;
@@ -1255,6 +1255,7 @@ public class NotificationPanelView extends PanelView implements
                 && y >= header.getTop() && y <= header.getBottom();
 
         final float w = getMeasuredWidth();
+
         float region = (w * (1.f/4.f)); // TODO overlay region fraction?
 
         boolean showQsOverride = false;
@@ -1618,8 +1619,11 @@ public class NotificationPanelView extends PanelView implements
         mLaunchAnimationEndRunnable = null;
         if (start) {
             mKeyguardBottomArea.launchPhone();
-        } else {
+        } else if (!mKeyguardBottomArea.isTargetCustom(
+                LockscreenShortcutsHelper.Shortcuts.RIGHT_SHORTCUT)) {
             mSecureCameraLaunchManager.startSecureCameraLaunch();
+        } else {
+            mKeyguardBottomArea.launchCamera();
         }
         mBlockTouches = true;
     }
@@ -1651,9 +1655,9 @@ public class NotificationPanelView extends PanelView implements
         });
         boolean start = getLayoutDirection() == LAYOUT_DIRECTION_RTL ? right : !right;
         if (start) {
-            mStatusBar.onPhoneHintStarted();
+            mStatusBar.onPhoneHintStarted(mKeyguardBottomArea.getLeftHint());
         } else {
-            mStatusBar.onCameraHintStarted();
+            mStatusBar.onCameraHintStarted(mKeyguardBottomArea.getRightHint());
         }
     }
 
